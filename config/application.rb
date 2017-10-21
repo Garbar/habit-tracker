fa6a000bd854203ea2f6d14a072954ef063772c2
Garbar <lorelsblog@gmail.com>
2017-10-21 11:19:47 +0300

    Initial clear code Add gems for specs

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,18 @@ module HabitTracker
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.resource_route false
+      generate.test_framework  :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: false,
+            request_specs: false
+       generate.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
